e5fd767dac6f8f2c277b4539994c8cbb8c8beba5
esalty <esaltydev@gmail.com>
2016-12-13 02:34:56 +0200

    -added  paySalaryToUsers and getUsersId methods to UserUtils class for 6.5 task

--- src/module6/User.java
+++ src/module6/User.java
@@ -5,11 +5,12 @@ package module6;
  */
 public class User {
 
-    private final long id;
-    private final String firstName;
-    private final String lastName;
-    private final int salary;
-    private final int balance;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    private int balance;
+
 
     public User(long id, String firstName, String lastName, int salary, int balance) {
         this.id = id;
@@ -18,6 +19,72 @@ public class User {
         this.salary = salary;
         this.balance = balance;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (salary != user.salary) return false;
+        if (balance != user.balance) return false;
+        if (!firstName.equals(user.firstName)) return false;
+        return lastName.equals(user.lastName);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + salary;
+        result = 31 * result + balance;
+        return result;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getBalance() {
+        return balance;
+
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 }
 
--- src/module6/UserUtils.java
+++ src/module6/UserUtils.java
@@ -4,4 +4,42 @@ package module6;
  * Created by GodMod on 12/12/2016.
  */
 public class UserUtils {
+    //    Создайте класс UserUtils с последующими методами:
+
+
+    static User[] uniqueUsers(User[] users) {
+
+        return users;
+    }
+    //users совпадают, когда все их поля совпадают
+
+
+    static User[] usersWithContitionalBalance(User[] users, int balance) {
+        return users;
+    }
+
+
+    //user’s balance == balance
+    static final User[] paySalaryToUsers(User[] users) {
+        for (User user : users) {
+            user.setBalance(user.getBalance() + user.getSalary());
+        }
+        return users;
+
+    }
+
+    static final long[] getUsersId(User[] users) {
+        long[] usersId = new long[users.length];
+        for (int i = 0; i < users.length; i++) {
+            usersId[i] = users[i].getId();
+        }
+        return usersId;
+        //return new long[users.length];
+    }
+
+
+    static User[] deleteEmptyUsers(User[] users) {
+        return users;
+    }
+
 }
